3e4a5ac82b70d2472d3a52417b79a2e7b8e270cd
SamHolmes1 <samband222@gmail.com>
2020-05-26 21:15:22 +0100

    Refactored litterally everything lmao

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const Discord = require('discord.js');
-
 const bot = new Discord.Client();
+const token = 'NzE0OTA5MzEyODI0MTE1Mjcx.Xs1hQQ.0BjlVmWe0GgO4i9q_1xNPhaW2E8';
 
-const token = 'NzE0ODIyMDQxMjk0OTk1NDU5.Xs0UNg.HNgBciAS3rMng9fTPWHvkRCyAYg'; //whatever the current token may be.
+const PREFIX = '!';
 
-bot.on('message', msg=>{
-	if(msg.content == "first rodeo"){
-		msg.reply('it aint his first rodeo.')
+const kane = '222095058436882443';
+const sam = '221779100258402305';
+
+bot.on('message', message=>{
+	let args = message.content.substring(PREFIX.length).split(" "); //args === any text after the prefix
+
+	switch(args[0]){ // switch statement for any commands
+		case 'check':
+			if (message.author.id === kane) {
+				message.reply('kane')
+			}
+			else if (message.author.id === sam) {
+				message.reply('sam')
+			}
+			break;
+
+	  case 'vibecheck':
+	  	message.channel.send({files: ["https://i.ytimg.com/vi/mYugwGNOIeM/maxresdefault.jpg"]});
+	  	break;
 	}
-
 })
 
-
-bot.on('message', message => {
-    console.log(message.content)
-    if (message.author.id === '222095058436882443') {
-        message.reply('i cant even tell how many rodeos youre on', { tts: true })
-    }
-	else if((message.author.id === '221779100258402305')){
-		message.reply('looks like youre on your first rodeo.', { tts: true })
-	}
+bot.on('message', message=>{
 
 })
-
-
 
 bot.login(token);
-
-bot.on('ready', () =>{
-	console.log('This bot is online!')
-})
